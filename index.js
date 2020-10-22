4cb59c16b56a2ce42f76400a7704176b5610597a
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-10-22 12:27:41 +0300

    I began doing everithing from scratch. Webpack removed, site concept will be fully changed. At this moment everithing is very raw

--- index.js
+++
@@ -1,2 +0,0 @@
-import './src/app';
-import './src/styles/style.css';
---
+++ src/data/Show.js
@@ -0,0 +1,9 @@
+export default class Show {
+    constructor(date, artist, city, place, cancelled) {
+        this.date = date;
+        this.artist = artist;
+        this.city = city;
+        this.place = place;
+        this.cancelled = cancelled;
+    }
+}
